225cc85d20481e2f69377fc4d7fe6998fc690cc1
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-06-19 21:50:23 +0300

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const htmlElement = document.querySelector("html");
 
-  const burgerMenu = document.querySelector(".burgerBtn");
-  const headerNav = document.querySelector(".header__nav");
-  const navLinks = document.querySelectorAll("nav a");
+	const burgerMenu = document.querySelector(".burgerBtn");
+	const headerNav = document.querySelector(".header__nav");
+	const navLinks = document.querySelectorAll("nav a");
 
-  burgerMenu.addEventListener("click", () =>
-    htmlElement.classList.toggle("open")
-  );
+	burgerMenu.addEventListener("click", () =>
+		htmlElement.classList.toggle("open")
+	);
 
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      htmlElement.classList.remove("open");
-    });
-  });
+	navLinks.forEach((link) => {
+		link.addEventListener("click", () => {
+		htmlElement.classList.remove("open");
+		});
+	});
+
+	document.querySelectorAll('a[href^="#"').forEach(link => {
+
+		link.addEventListener('click', function(e) {
+			e.preventDefault();
+	
+			let href = this.getAttribute('href').substring(1);
+	
+			const scrollTarget = document.getElementById(href);
+	
+			const topOffset = document.querySelector('header').offsetHeight;
+			const elementPosition = scrollTarget.getBoundingClientRect().top;
+			const offsetPosition = elementPosition - topOffset;
+	
+			window.scrollBy({
+				top: offsetPosition,
+				behavior: 'smooth'
+			});
+		});
+	});
+	
 });
